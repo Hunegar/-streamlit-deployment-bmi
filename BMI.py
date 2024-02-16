7e24b9a1d68910f160ad3d3b86f4119ad87ef691
Hunegar <160222977+Hunegar@users.noreply.github.com>
2024-02-16 18:06:48 +0800

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -13,7 +13,6 @@ st.title("Welocme to BMI calculator")
 #Input
 
 weight = st.number_input("Enter your weight in KG", step = 0.1)
-
 height = st.number_input("Enter your height in Meters", step = 0.01)
 
 def calculate_bmi():
@@ -28,8 +27,20 @@ def calculate_bmi():
         level = level_labels[2]
     else:
         level = level_labels[3]
-    st. success(f"Your BMI is {bmi}. You are at {level}")
+    st.success(f"Your BMI is {bmi}. You are at {level}")
 
-button = st.button("Calculate BMI")
-if button:
-    calculate_bmi()
+check = True
+while check == True:
+    if height > 2.7 or height < 0.55:
+        st.success("Height is not human!")
+        check = False
+        break
+    elif weight < 20 or weight > 200:
+        st.success("Weight is not human!")
+        check = False
+        break
+    else:
+        calculate_bmi(weight,height)
+        button = st.button("Calculate BMI")
+        if button:
+            calculate_bmi()
